27529e83371832eb4127b75cd873d1b7edc9817e
Zax71 <67716263+zax71@users.noreply.github.com>
2025-11-22 17:54:49 +0000

    Rename project

--- flake.nix
+++ flake.nix
@@ -7,12 +7,22 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rust-overlay,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs { inherit system overlays; };
-      in with pkgs; {
+      in
+      with pkgs;
+      {
         devShells.default = mkShell rec {
           buildInputs = [
             # Rust
@@ -37,9 +47,13 @@
             xorg.libXi
             xorg.libX11
 
+            # Dev tools
+            act # Run GitHub Actions locally
+
           ];
 
           LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
         };
-      });
+      }
+    );
 }
